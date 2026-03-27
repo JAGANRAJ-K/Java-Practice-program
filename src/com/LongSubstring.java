5ce8c9921300fae8336d9c5ca79ef041aa2ef963
JAGANRAJ-K <jaganrajk000@gmail.com>
2026-03-27 13:33:57 +0530

    check 1

--- src/com/LongSubstring.java
+++ src/com/LongSubstring.java
@@ -6,5 +6,6 @@ public class LongSubstring {
 	public static void main(String[] args) {
 		String []a= {"flower","flower","flower"};
 		System.out.println(Arrays.toString(a));
+		System.out.println("yes it is");
 	}
 }
